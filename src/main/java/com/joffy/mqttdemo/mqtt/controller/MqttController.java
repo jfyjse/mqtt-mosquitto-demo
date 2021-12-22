69e8964089c30add0adefeaa98fbf5e113091850
Joffy Jose <joffy.jose.jfy@gmail.com>
2021-12-22 17:46:01 +0530

    mqtt basic send and recieve

---
+++ src/main/java/com/joffy/mqttdemo/mqtt/controller/MqttController.java
@@ -0,0 +1,30 @@
+package com.joffy.mqttdemo.mqtt.controller;
+import com.google.gson.Gson;
+import com.google.gson.JsonObject;
+import com.joffy.mqttdemo.mqtt.MqttGateway;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.messaging.handler.annotation.Header;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class MqttController {
+    @Autowired
+    MqttGateway mqtGateway;
+    @PostMapping("/sendMessage")
+    public ResponseEntity<?> publish(@RequestBody String mqttMessage){
+
+        try {
+            JsonObject convertObject = new Gson().fromJson(mqttMessage, JsonObject.class);
+           // mqtGateway.senToMqtt(convertObject.get("message").toString(), convertObject.get("topic").toString());
+            mqtGateway.senToMqtt(convertObject.get("message").getAsString(),convertObject.get("topic").getAsString());
+            return ResponseEntity.ok("Success");
+        }catch(Exception ex) {
+            ex.printStackTrace();
+            return ResponseEntity.ok("fail");
+        }
+    }
+
+}
---
+++ src/test/java/com/joffy/mqttdemo/MqttDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.joffy.mqttdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MqttDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
