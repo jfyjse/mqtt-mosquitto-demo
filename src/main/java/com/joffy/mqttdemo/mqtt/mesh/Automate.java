a2b4ddd04a66d569d88a3ab4ad13b4837b30cf62
Joffy Jose <=joffy@sayonetech.com>
2023-05-15 13:06:42 +0530

    updated

--- src/main/java/com/joffy/mqttdemo/mqtt/mesh/Automate.java
+++ src/main/java/com/joffy/mqttdemo/mqtt/mesh/Automate.java
@@ -1,4 +1,5 @@
 package com.joffy.mqttdemo.mqtt.mesh;
+
 import com.joffy.mqttdemo.mqtt.MqttGateway;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
@@ -12,11 +13,15 @@ public class Automate {
     @Autowired
     MqttGateway mqttGateway;
 
-    @Scheduled(fixedDelay = 10000)
+    @Scheduled(fixedDelay = 5000)
     public void meshMock() {
         try {
-            mqttGateway.senToMqtt(msg_data, "mqtt-test");
-//            mqttGateway.senToMqtt(msg_data, "elevator-02");
+            int i=0;
+            //mqttGateway.senToMqtt("[{\"timestamp\":\"2022-06-17T02:03:51Z\",\"type\":\"iBeacon\",\"mac\":\"10C005091CD5\",\"bleName\":\"\",\"ibeaconUuid\":\"20354D7AE4FE47AF8FF6187BCA92F3F9\",\"ibeaconMajor\":1,\"ibeaconMinor\":1,\"rssi\":-50,\"ibeaconTxPower\":-70,\"battery\":0}]\n", "test");
+            mqttGateway.senToMqtt(msg_data, "1");
+            //10000 ble unique
+            //500 mesh unique
+            //1 mesh 20 ble
 
         } catch (Exception ex) {
             ex.printStackTrace();
